4ec41d04d4e8aa1f4ff29d9d1843a4446510af6e
jie <894418689@qq.com>
2022-04-21 13:56:32 +0800

    test 2

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author zhijie.huang
+ * @create 2022-04-21 13:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("test for git");
+        System.out.println("test for git2");
+    }
+}
